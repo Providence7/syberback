04f4a63e664bf43498685b58e4917448ded991fb
Providence7 <afuyeuthman2018@gmail.com>
2025-06-29 07:32:52 -0400

    change

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: 'https://syber.onrender.com/', // your frontend origin
+  origin: 'https://syber.onrender.com', // your frontend origin
   credentials: true,
 }));
 
